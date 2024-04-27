309d833a56c20aa6f0787d60da3b8d054cea2e4d
Michael-Starrk <michaelschimittpro@gmail.com>
2024-04-26 22:45:55 -0300

    project Alpha 1.2

--- src/services/Registration.java
+++ src/services/Registration.java
@@ -1,4 +1,19 @@
 package services;
 
+import textTerminal.textRegistration;
+
+import java.util.Scanner;
+
 public class Registration {
+    textRegistration text = new textRegistration();
+    Scanner scanner = new Scanner(System.in);
+    private String name;
+    private String email;
+    private String password;
+    private String countryOfOrigin;
+
+    public void registrationOfAccount(){
+        text.printText();
+
+    }
 }
--- src/textTerminal/textRegistration.java
+++ src/textTerminal/textRegistration.java
@@ -1,10 +1,41 @@
 package textTerminal;
 
+import java.util.Scanner;
+
 public class textRegistration {
+    Scanner scanner = new Scanner(System.in);
+    private String name;
+    private String email;
+    private String password;
+    private String countryOfOrigin;
 
     public void printText(){
-        System.out.println("To register, simply fill in the details below.\n" +
-                            "New users get a 30% bonus on STK points.");
+        System.out.print("To register, simply fill in the details below.\n" +
+                            "New users get a 30% bonus on STK points.\n\n" +
+                            "Name: ");
+        name = scanner.nextLine();
+        System.out.print("Email: \n");
+        email = scanner.nextLine();
+
+        System.out.print("The password must be strong. (Letters, numbers, minimum 8 characters)\n" +
+                            "password: ");
+        password = scanner.nextLine();
+        System.out.print("Country of origin: \n");
+        countryOfOrigin = scanner.nextLine();
+    }
+    public String getName() {
+        return name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
 }
