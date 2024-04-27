40395cf3fe69d3e4d68a94b7ea9bebf47ca14a89
Michael-Starrk <michaelschimittpro@gmail.com>
2024-04-26 23:12:58 -0300

    project Alpha 1.3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import services.ValidateCPF;
 import textTerminal.textLoginOrRegistration;
 import textTerminal.textWelcomeSTK;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         textWelcomeSTK welcome = new textWelcomeSTK();
         textLoginOrRegistration welcome1 = new textLoginOrRegistration();
+        ValidateCPF validate = new ValidateCPF();
         welcome.printText();
-        welcome1.printText();
+        System.out.print("CPF: \n");
+        String cpf = scanner.nextLine();
+        System.out.print("O CPF é valido? " + validate.valid(cpf));
     }
 }
--- src/services/ValidateCPF.java
+++ src/services/ValidateCPF.java
@@ -1,4 +1,55 @@
 package services;
 
 public class ValidateCPF {
+    Registration registration = new Registration();
+    public static boolean valid(String cpf){
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        if (cpf.length() != 11 || cpf.matches(cpf.charAt(0) + "{11}")) {
+            return false;
+        }
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        try {
+            sm = 0;
+            peso = 10;
+            for (i = 0; i < 9; i++) {
+                num = (int) (cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig10 = '0';
+            } else {
+                dig10 = (char) (r + 48);
+            }
+
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = (int) (cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig11 = '0';
+            } else {
+                dig11 = (char) (r + 48);
+            }
+
+            if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10))) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
